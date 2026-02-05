8c07491e55e65c34a8b4c97dc00472abd715b27d
bob-jr-kab <kanyindajr520@gmail.com>
2026-02-05 17:09:55 +0200

    Format firebaseConfig.js for consistency

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -5,8 +5,9 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // 👇 NO NEED for databaseURL if you're using Firestore
+  
 });
 
 const firestore = admin.firestore();
 module.exports = firestore;
+
